edf3366fd6d19d9c7ab68298eb33e3f077fe3642
Riley Littlefield <rileylittlefield@ymail.com>
2019-01-17 00:09:00 -0500

    begins utilizing data frames for data visualization

---
+++ src/data_management/training_dataframe.py
@@ -0,0 +1,27 @@
+from pandas import DataFrame
+from data_management.load_training import titanic_set_labelled
+
+as_columns = {
+    column_name: []
+    for column_name
+    in titanic_set_labelled[0].keys()
+}
+
+def adapt_age(age):
+    return float(age) if age else age;
+
+for passenger in titanic_set_labelled:
+    for col_name, value in passenger.items():
+        if col_name == 'Age':
+            as_columns.get(col_name).append(adapt_age(value))
+        else:
+            as_columns.get(col_name).append(value)
+
+passengers = DataFrame(data=as_columns)
+
+# ages = passengers.Age
+
+# not_having_age = passengers[passengers.Age == '']
+# having_age = passengers[passengers.Age != '']
+# print(len(having_age))
+# print(len(not_having_age))
--- src/data_visualization/age_stats.py
+++ src/data_visualization/age_stats.py
@@ -1,40 +1,33 @@
 from data_management.pickers import count_passengers, is_any, pick_age
 from data_management.load_training import titanic_set_labelled
+from data_management.training_dataframe import passengers
 
 print('STATS ON AGE', '\n')
 
-passenger_count = count_passengers(is_any)
-print('LABELLED TOTAL = ', passenger_count, '\n')
+print('LABELLED TOTAL = ', len(passengers), '\n')
 
-missing_age = [
-    passenger
-    for passenger
-    in titanic_set_labelled
-    if not passenger['Age']
-]
+missing_age = passengers[passengers.Age == '']
 
-missing_age_count = len(missing_age)
+print('NUMBER OF PASSENGERS MISSING AGE = ', len(missing_age))
+print('PROPORTION PASSENGERS MISSING AGE = ', round(len(missing_age) / len(passengers), 3), '\n')
 
-having_age = [
-    passenger
-    for passenger
-    in titanic_set_labelled
-    if passenger['Age']
-]
+having_age = passengers[passengers.Age != '']
 
-having_age_count = len(having_age)
+print('NUMBER OF PASSENGERS WITH AGE = ', len(having_age))
+print('PROPORTION PASSENGERS WITH AGE = ', round(len(having_age) / len(passengers), 3), '\n')
 
-print('NUMBER OF PASSENGERS MISSING AGE = ', missing_age_count)
-print('PROPORTION PASSENGERS MISSING AGE = ', round(missing_age_count / passenger_count, 3), '\n')
+print('youngest age = ', having_age.Age.min())
 
-print('NUMBER OF PASSENGERS WITH AGE = ', having_age_count)
-print('PROPORTION PASSENGERS WITH AGE = ', round(having_age_count / passenger_count, 3), '\n')
+print('oldest age = ', having_age.Age.max(), '\n')
 
-youngest_passenger = min(having_age, key=pick_age)
-print('youngest age = ', youngest_passenger['Age'])
+# mean_age = sum(map(pick_age, having_age), 0) / having_age_count
+# print('mean age = ', mean_age)
 
-oldest_passenger = max(having_age, key=pick_age)
-print('oldest age = ', oldest_passenger['Age'], '\n')
+# median_age = (
+#     (
+#         pick_age(having_age_sorted[int(having_age_count /2)]) + 
+#         pick_age(having_age_sorted[int(having_age_count / 2 - 1)])
+#     ) / 2
+# )
+# print('median age = ', median_age)
 
-mean_age = sum(map(pick_age, having_age), 0) / having_age_count
-print('mean age = ', mean_age)
