7017cb846544e85becac18313df835d42521cf9e
Riley Littlefield <rileylittlefield@ymail.com>
2019-01-19 20:34:32 -0500

    refactors gender_stats to utilize pandas DataFrame

--- src/data_visualization/gender_stats.py
+++ src/data_visualization/gender_stats.py
@@ -1,25 +1,27 @@
 from data_management.pickers import count_passengers, is_male, is_female, survived, is_any
+from data_management.training_dataframe import passengers
 from functools import reduce
 
 print('STATS ON SEX', '\n')
 
 labelled_total = count_passengers(is_any)
-print('TOTAL LABELLED ENTRIES: ', labelled_total, '\n')
 
-male_count = count_passengers(is_male)
-print('TOTAL MALES = ', male_count)
+print('TOTAL LABELLED ENTRIES: ', len(passengers), '\n')
 
-surviving_males = count_passengers(lambda passenger: is_male(passenger) and survived(passenger))
-print('SURVIVING MALES = ', surviving_males)
+male_passengers = passengers[passengers.Sex == 'male']
+print('TOTAL MALES = ', len(male_passengers))
 
-surviving_male_ratio = surviving_males / male_count
+surviving_males = male_passengers[male_passengers.Survived == '1']
+print('SURVIVING MALES = ', len(surviving_males))
+
+surviving_male_ratio = len(surviving_males) / len(male_passengers)
 print('SURVIVING MALES RATIO = ', round(surviving_male_ratio, 3), '\n')
 
-female_count = count_passengers(is_female)
-print('TOTAL FEMALES = ', female_count)
+female_passengers = passengers[passengers.Sex == 'female']
+print('TOTAL FEMALES = ', len(female_passengers))
 
-surviving_female_count = count_passengers(lambda passenger: is_female(passenger) and survived(passenger))
-print('TOTAL SURVIVING FEMALES = ', surviving_female_count)
+surviving_females = female_passengers[female_passengers.Survived == '1']
+print('TOTAL SURVIVING FEMALES = ', len(surviving_females))
 
-surviving_female_ratio = surviving_female_count / female_count
+surviving_female_ratio = len(surviving_females) / len(female_passengers)
 print('SURVIVING FEMALES RATIO = ', round(surviving_female_ratio, 3), '\n')
