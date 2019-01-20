5d70ae37aaf258eccd7463371541a47be007e3cd
Riley Littlefield <rileylittlefield@ymail.com>
2019-01-19 20:53:21 -0500

    adds dispersion stats for passenger age

--- src/data_visualization/age_stats.py
+++ src/data_visualization/age_stats.py
@@ -24,3 +24,24 @@ print('1st quartile = ', having_age.Age.quantile(.25))
 print('median age = ', having_age.Age.median())
 print('3rd quartile = ', having_age.Age.quantile(.75), '\n')
 
+print('SURVIVOR DISPERSION', '\n')
+
+survivors = having_age[having_age.Survived == '1']
+print('youngest age = ', survivors.Age.min())
+print('oldest age = ', survivors.Age.max())
+print('mean age = ', survivors.Age.mean(), '\n')
+
+print('first quartile = ', survivors.Age.quantile(.25))
+print('median = ', survivors.Age.median())
+print('third quartile = ', survivors.Age.quantile(.75), '\n')
+
+print('CASUALTY DISPERSION', '\n')
+
+casualities = having_age[having_age.Survived == '0']
+print('youngest age = ', casualities.Age.min())
+print('oldest age = ', casualities.Age.max())
+print('mean age = ', casualities.Age.mean(), '\n')
+
+print('first quartile = ', casualities.Age.quantile(.25))
+print('median age = ', casualities.Age.median())
+print('third quartile = ', casualities.Age.quantile(.75), '\n')
