1be43f686fd95ec721be56f9f5e2fdef44882f42
Riley Littlefield <rileylittlefield@ymail.com>
2019-01-22 01:15:37 -0500

    prints sibsp stats for different labels

--- src/data_visualization/sibsp_stats.py
+++ src/data_visualization/sibsp_stats.py
@@ -2,3 +2,15 @@ from data_management.training_dataframe import passengers
 from reusable_stats_builders.dispersion import print_dispersion_stats
 
 print_dispersion_stats(passengers, 'SIBLING SPOUSE COUNT', 'SibSp')
+
+print_dispersion_stats(
+    passengers[passengers.Survived == '1'],
+    'SIBLING SPOUSE COUNT SURVIVORS',
+    'SibSp'
+)
+
+print_dispersion_stats(
+    passengers[passengers.Survived == '0'],
+    'SIBLING SPOUSE COUNT CASUALTIES',
+    'SibSp'
+)
